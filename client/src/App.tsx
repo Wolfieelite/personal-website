531faaf13988ac0325b72e1884b33c6f291d9840
wolfieelite <martetaylor333@gmail.com>
2025-06-19 12:13:11 -0700

    initalized tailwind css, tested and success

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      
+      <div className="bg-amber-950">hello world</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

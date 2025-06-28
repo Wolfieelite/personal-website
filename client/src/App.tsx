2964beb97baf78bc850cf6edef5f1f0c1f1d485c
wolfieelite <martetaylor333@gmail.com>
2025-06-28 09:40:54 -0700

    updated initialzed

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className="text-9xl">Hello world</div>
+    </>
+  )
+}
+
+export default App
